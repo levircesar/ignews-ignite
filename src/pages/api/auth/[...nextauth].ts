7e9b66ef205c0692c136546d0b7ab8c996477654
levircesar <levirlemos@gmail.com>
2022-02-27 09:53:36 -0300

    verificando usuario duplicado

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from "../../../services/fauna";
+import { query as q } from 'faunadb'
 
 export default NextAuth({
   providers: [
@@ -13,4 +15,46 @@ export default NextAuth({
       },
     }),
   ],
+  
+  
+  secret: process.env.SIGNIN_KEY,
+    
+  pages: {
+    signIn: process.env.NEXTAUTH_URL + '/',
+    signOut:  process.env.NEXTAUTH_URL + '/',
+    error:  process.env.NEXTAUTH_URL + '/', // Error code passed in query string as ?error=
+  },
+  callbacks : {
+    async signIn(data) {
+      const { email } = data.user;
+      try{
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(data.user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(data.user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch(error) {
+        console.log(error)
+        return false
+      }
+    }
+  }
 })
