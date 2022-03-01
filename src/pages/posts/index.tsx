bf8be692981bcc4f640fb933d757b0837852db40
levircesar <levirlemos@gmail.com>
2022-02-28 21:14:26 -0300

    listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,22 @@
 import Head from "next/head";
 import styles from "./styles.module.scss";
 import Prismic from "@prismicio/client"
+import { RichText } from 'prismic-dom'
 import { GetStaticProps } from "next";
 import { getPrismicClient } from "../../services/prismic";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps{
+  posts: Post[]
+}
+
+export default function Posts({posts} : PostsProps) {
   return (
     <>
       <Head>
@@ -13,42 +25,15 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Oportunidades internacionais em programação</strong>
-            <p>
-              Obter uma oportunidade no exterior é um objetivo e um sonho para
-              muitos devs que procuram expandir o conhecimento através de
-              experiências multiculturais. Apesar de haver uma série de desafios
-              — que vão desde a qualificação profissional até a bagagem de
-              conhecimento exigida lá fora — muitas pessoas desenvolvedoras
-              conseguem abrir as fronteiras em seus currículos.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Oportunidades internacionais em programação</strong>
-            <p>
-              Obter uma oportunidade no exterior é um objetivo e um sonho para
-              muitos devs que procuram expandir o conhecimento através de
-              experiências multiculturais. Apesar de haver uma série de desafios
-              — que vão desde a qualificação profissional até a bagagem de
-              conhecimento exigida lá fora — muitas pessoas desenvolvedoras
-              conseguem abrir as fronteiras em seus currículos.
-            </p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong>Oportunidades internacionais em programação</strong>
-            <p>
-              Obter uma oportunidade no exterior é um objetivo e um sonho para
-              muitos devs que procuram expandir o conhecimento através de
-              experiências multiculturais. Apesar de haver uma série de desafios
-              — que vão desde a qualificação profissional até a bagagem de
-              conhecimento exigida lá fora — muitas pessoas desenvolvedoras
-              conseguem abrir as fronteiras em seus currículos.
-            </p>
-          </a>
+          {posts.map(post => (
+            <a href="#" key={post.slug}>
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>
+                {post.excerpt}
+              </p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -59,7 +44,7 @@ export default function Posts() {
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient()
 
-  const response = await prismic.query([
+  const response = await prismic.query<any>([
     Prismic.predicates.at('document.type','post')
   ], {
     fetch: ['post.title', 'post.content'],
@@ -68,7 +53,21 @@ export const getStaticProps: GetStaticProps = async () => {
   )
   console.log(response)
   // console.log(JSON.stringify(response, null , 2))
+  const posts = response.results.map(post => {
+    return {
+      slug:post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type ==='paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+
+        month: 'long',
+        year: 'numeric'
+      })
+
+    }
+  })
   return {
-    props:{}
+    props:{posts}
   }
 }
